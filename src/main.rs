5dbbb0af31d6a315a6a80d787ae9872c69c904b4
Simon Brummer <simon.brummer@posteo.de>
2021-06-18 09:47:58 +0200

    Test with real LEDs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{thread, time};
+use rust_raspi_led_strip::APA102;
+use blink_dbus_server::BlinkDbusService;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting BlinkDbusService!");
+
+    let mut srv = BlinkDbusService::<APA102>::new();
+    srv.start(APA102::new(50));
+
+    loop {
+        thread::sleep(time::Duration::from_millis(5000));
+    }
 }
